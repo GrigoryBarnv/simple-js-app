d51cd3b2b5dc0be16007dca7f0b1ea95b2eb4561
root <root@LAPTOP-B6448FIR>
2025-05-11 00:47:39 +0200

    Initialize pokemon list with basic structure

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// scripts.js
+
+// Create an empty array to hold Pokémon data
+let pokemonList = [];
+
+// Add Pokémon objects to the array
+pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Charmander',
+    height: 6,
+    types: ['fire']
+  },
+  {
+    name: 'Squirtle',
+    height: 5,
+    types: ['water']
+  }
+];
+
+// Log the list to verify it works
+console.log(pokemonList);
