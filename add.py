9ca49062f826234347c0e592257feff370ded544
GrigoryBarnv <grigorybaranov2@gmail.com>
2025-06-21 19:08:12 +0200

    python new function

--- add.py
+++ add.py
@@ -1,19 +1,51 @@
-import math
+fruit = "banana"
+len(fruit)
+length = len(fruit)
+fruit[length - 1]
+fruit[-1 ]
 
-def estimate_pi():
-    total = 0
-    k = 0
-    factor = (2 * math.sqrt(2)) / 9801
+fruit[5:3]
 
-    while True:
-        num = math.factorial(4*k) * (1103 + 26390*k)
-        denom = math.factorial(k)**4 * 396**(4*k)
-        term = factor * num / denom
-        total += term
 
-        if abs(term) < 1e-15:
-            break
 
-        k += 1
 
-    return 1 / total
+## add 3rd parameter where it should start loooking 
+def find(word, letter, start_index):
+    index = start_index
+    while index < len(word):
+        if word[index] == letter:
+            return index
+        index = start_index + 1
+    return print("Letter not found")
+
+
+def find(word, letter, start_index):
+    index = start_index
+    while index < len(word):
+        if word[index] == letter:
+            return index
+        index += 1  # increment properly
+    print("Letter not found")
+    return -1  # return -1 if not found
+
+
+
+
+# 3 parameter + encapsulate in function
+def count(word, letter, start_index):
+    count = 0
+    start_index = abs(start_index)
+    for letter in word:
+        if letter == 'a':
+            start_index = count + 1
+        print(count)
+
+
+word = 'Pinapple'
+if word < 'banana':       
+print('Your word, ' + word + ', comes before banana.')
+elif word > 'banana':
+print('Your word, ' + word + ', comes after banana.')
+else:
+print('All right, bananas.')
+                    
