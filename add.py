3bca9ee6ba9d5c8e9ac2daa02c41d1820756bb45
GrigoryBarnv <grigorybaranov2@gmail.com>
2025-06-22 17:03:10 +0200

    python app update

--- add.py
+++ add.py
@@ -1,51 +1,13 @@
-fruit = "banana"
-len(fruit)
-length = len(fruit)
-fruit[length - 1]
-fruit[-1 ]
+s = 'spam'
+t = list(s)
+t = list('spam')
 
-fruit[5:3]
+del list
+s = 'gasd asd aasf'
+t = s.split()
+t
 
+s = 'dadd-sdds-dsds'
+delimiter = ' '
+t = s.split(delimiter)
 
-
-
-## add 3rd parameter where it should start loooking 
-def find(word, letter, start_index):
-    index = start_index
-    while index < len(word):
-        if word[index] == letter:
-            return index
-        index = start_index + 1
-    return print("Letter not found")
-
-
-def find(word, letter, start_index):
-    index = start_index
-    while index < len(word):
-        if word[index] == letter:
-            return index
-        index += 1  # increment properly
-    print("Letter not found")
-    return -1  # return -1 if not found
-
-
-
-
-# 3 parameter + encapsulate in function
-def count(word, letter, start_index):
-    count = 0
-    start_index = abs(start_index)
-    for letter in word:
-        if letter == 'a':
-            start_index = count + 1
-        print(count)
-
-
-word = 'Pinapple'
-if word < 'banana':       
-print('Your word, ' + word + ', comes before banana.')
-elif word > 'banana':
-print('Your word, ' + word + ', comes after banana.')
-else:
-print('All right, bananas.')
-                    
