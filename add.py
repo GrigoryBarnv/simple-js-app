e1800db85da962c19dd035c88674a3cd4d9b6342
GrigoryBarnv <grigorybaranov2@gmail.com>
2025-06-25 16:08:06 +0200

    changes java script

--- add.py
+++ add.py
@@ -1,21 +1,68 @@
-import sys
+class Time:
+ """Represents the time of day.
+ attributes: hour, minute, second
+ """
+    def __init__(self, hour=0, minute=0, second=0):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
 
-def sed(pattern, replacement, source, target):
-    try:
-        with open(source, 'r') as src_file, \
-             open(target, 'w') as tgt_file:
-            for line in src_file:
-                new_line = line.replace(pattern, replacement)
-                tgt_file.write(new_line)
+time = Time()
+time.hour = 11
+time.minute = 59
+time.second = 45
 
-    except IOError as e:
-        print(f"Error: {e}")
-        sys.exit(1)
 
-    except FileNotFoundError as e:
-        print(f"Error: {e}")
-        sys.exit(1)
+def add_time(t1, t2):
+    sum = Time()
+    sum.hour = t1.hour + t2.hour
+    sum.minute = t1.minute + t2.minute
+    sum.second = t1.second + t2.second
+    return sum
 
-if __name__ == '__main__':
-    
-                
+
+
+start = Time()
+start.hour = 9
+start.minute = 45
+start.second = 0
+
+duration = Time()
+duration.hour = 1
+duration.minute = 35
+duration.second = 0
+
+
+def print_time(t):
+    print('%.2d:%.2d:%.2d' % (t.hour, t.minute, t.second))
+
+
+
+done = add_time(start, duration)
+print_time(done)
+
+def add_time(t1, t2):
+    sum = Time()
+    sum.hour = t1.hour + t2.hour
+    sum.minute = t1.minute + t2.minute
+    sum.second = t1.second + t2.second
+
+    if sum.second >= 60:
+        sum.minute += 1
+        sum.second -= 60
+
+    if sum.minute >= 60:
+        sum.hour += 1
+        sum.minute -= 60
+
+
+def time_to_int(time):
+    minutes = time.hour * 60 + time.minute
+    seconds = minutes * 60 + time.second
+    return seconds
+
+def int_to_time(seconds):
+    time = Time()
+    minutes, time.second = divmod(seconds, 60)
+    time.hour, time.minute = divmod(minutes, 60)
+    return time
